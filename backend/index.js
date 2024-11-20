ee1b57c12e4ec0c0bdea14af0ac5316b1dd35776
M-S-R-P <sivaramprasad.maddukuri@gmail.com>
2024-11-20 11:25:07 +0530

    Add login route and authenticate middleware

---
+++ backend/index.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const cors = require("cors");
+const { jwt } = require("jsonwebtoken");
+const pool = require("./db");
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+app.listen(5000, () => {
+    console.log("Listening on port 5000");
+});
+
+// Login method
+app.post("/login", async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        const { rows } = await pool.query(
+            "SELECT * users WHERE username = $1",
+            [username]
+        );
+        if (rows.length === 0 || password != rows[0].password) {
+            console.log("Invalid Username/Password", rows.length);
+            return res.status(401).json({ error: "Invalid Username/Password" });
+        }
+        const token = jwt.sign({ id: rows[0].id }, "your_secret_key", {
+            expiresIn: "1h",
+        });
+        res.json({ token });
+    } catch (err) {
+        res.status(403).json({ error: "DBError" });
+        console.log(err);
+    }
+});
+
+// Authentication Middleware
+
+const authenticate = (req, res, next) => {
+    const token = req.headers.authorization.split(" ")[1];
+    if (!token) {
+        return res.status(401).json({ error: "No token provided" });
+    }
+    jwt.verify(token, "your_secret_key", async (err, decoded) => {
+        if (err) {
+            console.log(err);
+            return res.status(403).json({ error: "Failed to verify token" });
+        }
+        try {
+            const { rows } = await pool.query(
+                "SELECT * FROM users WHERE id = $1",
+                [decoded.id]
+            );
+            if (rows.length === 0) {
+                return res.status(403).json({ error: "Invalid user" });
+            }
+            req.user = rows[0];
+            next();
+        } catch (err) {
+            console.log(err);
+            return res.status(403).json({ error: "DB Error" });
+        }
+    });
+};
