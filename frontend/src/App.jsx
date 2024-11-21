32b64e77fbfc76f390c43bbec139b6bd78132337
M-S-R-P <sivaramprasad.maddukuri@gmail.com>
2024-11-21 13:30:26 +0530

    Add LoginComponent

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,30 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { LoginComponent } from "./components/LoginComponent";
+import axios from "axios";
 
 function App() {
-  
-  return (
-    <></>
-  )
+  const [isLoggedIn, setIsLoggedIn] = useState(() => {
+    return localStorage.getItem("token") ? true : false;
+  });
+
+  const [bills, setBills] = useState([]);
+
+  const handleLogin = async (username, password) => {
+    try {
+      const response = await axios.post("http://localhost:5000/login", {
+        username,
+        password,
+      });
+      localStorage.setItem("token", response.data.token);
+      setIsLoggedIn(true);
+    } catch (err) {
+      window.alert("Incorrect Username/Password");
+      console.log(err);
+    }
+  };
+
+  return <LoginComponent onLogin={handleLogin} />;w
 }
 
-export default App
+export default App;
